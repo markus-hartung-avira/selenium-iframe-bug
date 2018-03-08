861c1e47b6bafb64dfcd2952ab4f91965226b6bf
Markus Hartung <markus.hartung@avira.com>
2018-03-08 15:00:38 +0100

    Demonstrate that sendKeys does not work in extension frames

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # selenium-iframe-bug
 Demonstrates a bug when switching to an iframe with extension src
 
+Run test with:
+```node test/main.js```
 
--- test/main.js
+++ test/main.js
@@ -11,7 +11,7 @@ function startServer() {
   console.log('start server');
   app = express();
   app.use(express.static(__dirname + '/../public/'));
-  return new Promise((resolve) => {  
+  return new Promise((resolve) => {
     server = app.listen(8080, resolve);
   }).then(() => console.log('server started'));
 }
@@ -38,16 +38,21 @@ function switchToFrame(id) {
     return driver.switchTo().frame(element)
       .then(() => console.log('switch done:', id))
       .catch(e => console.error('switch error:', e.message))
+      .then(typeSomething)
       .then(() => driver.switchTo().defaultContent());
   }).catch((e) => console.error('could not find element', id));
 }
 
+function typeSomething() {
+  return driver.findElement({ css: 'input' }).then((element) => {
+    element.sendKeys('I was here');
+  })
+}
+
 startServer()
   .then(() => driver.get('http://localhost:8080/index.html'))
+  .then(typeSomething)
   .then(() => switchToFrame('normalSrc'))
   .then(() => switchToFrame('extensionSrc'))
-  .then(() => switchToFrame('normalSrcAddedByExtension'))
-  .then(() => switchToFrame('extensionSrcAddedByExtension'))
-  .then(shutdownServer);
-
-
+  .then(typeSomething);
+  //.then(shutdownServer);
